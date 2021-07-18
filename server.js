af98267136488d23bbbc8b10455e072a3a8aa111
JeffCW <changcheewah96@gmail.com>
2021-07-18 09:25:54 +0800

    update to production version

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const DB = require('./routes/db');
 const app = express();
+const corsOptions = {
+    origin: 'https://travel-kaki.netlify.app',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+app.use(cors(corsOptions));
+// app.use(cors())
 require('dotenv').config();
 app.use(express.json({ extended: false }));
-app.use(cors());
+
 app.use("/api", require("./routes/google"));
 app.use("/user", require("./routes/auth"));
 app.use(require("./routes/user"))
